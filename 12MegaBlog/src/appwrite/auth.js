aa04ac26bd3fa5d88ca98f276c60eae2b1fa7fd5
Rishuraj Pandey <rishu04112004@gmail.com>
2026-04-22 23:15:16 +0530

    made btn and index.js

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,5 +1,5 @@
-import conf from "./conf/conf";
-import { Client, Account } from "appwrite";
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
     client = new Client();
@@ -56,6 +56,6 @@ export class AuthService {
 
 const authService = new AuthService();
 
-export default AuthService;
+export default authService;
 
 // authService.client = new Client() 
